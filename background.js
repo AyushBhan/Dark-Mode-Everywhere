205258485ee1bb712b7273b01e57bfc06ae67404
AyushBhan <126169474+AyushBhan@users.noreply.github.com>
2023-02-23 10:17:01 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,10 @@
+// Listen for the browser action button to be clicked
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.insertCSS({file: "styles.css"});
+
+  // Toggle the "dark-mode" class on the body element
+  chrome.tabs.executeScript(tab.id, {
+    code: `
+      document.body.classList.toggle("dark-mode");
+    `
+  });
 });
